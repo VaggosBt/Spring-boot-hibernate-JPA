59953e0b344c46254340ecde19c7be73964ec6d1
VaggosBt <vaggelisbit@hotmail.com>
2023-06-28 15:04:34 +0300

    Initial Commit

---
+++ src/test/java/udemy/courses/jpa/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package udemy.courses.jpa.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
